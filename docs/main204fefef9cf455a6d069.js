c4cb2c45717722a9ab313df1e4080dedf5d93a58
lUCI <lusindisontanjana@gmail.com>
2022-05-18 14:14:01 +0200

    implement card

---
+++ docs/main204fefef9cf455a6d069.js
@@ -0,0 +1,25 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nError [ERR_MODULE_NOT_FOUND]: Cannot find package '@babel/plugin-transform-runtime' imported from /home/luci/Documents/microverse/week5/Javascript-Capstone/babel-virtual-resolve-base.js\n    at new NodeError (/home/luci/Documents/microverse/week5/Javascript-Capstone/node_modules/@babel/core/lib/vendor/import-meta-resolve.js:2552:5)\n    at packageResolve (/home/luci/Documents/microverse/week5/Javascript-Capstone/node_modules/@babel/core/lib/vendor/import-meta-resolve.js:3208:9)\n    at moduleResolve (/home/luci/Documents/microverse/week5/Javascript-Capstone/node_modules/@babel/core/lib/vendor/import-meta-resolve.js:3242:18)\n    at defaultResolve (/home/luci/Documents/microverse/week5/Javascript-Capstone/node_modules/@babel/core/lib/vendor/import-meta-resolve.js:3281:13)\n    at /home/luci/Documents/microverse/week5/Javascript-Capstone/node_modules/@babel/core/lib/vendor/import-meta-resolve.js:3304:14\n    at Generator.next (<anonymous>)\n    at asyncGeneratorStep (/home/luci/Documents/microverse/week5/Javascript-Capstone/node_modules/@babel/core/lib/vendor/import-meta-resolve.js:63:103)\n    at _next (/home/luci/Documents/microverse/week5/Javascript-Capstone/node_modules/@babel/core/lib/vendor/import-meta-resolve.js:65:194)\n    at /home/luci/Documents/microverse/week5/Javascript-Capstone/node_modules/@babel/core/lib/vendor/import-meta-resolve.js:65:364\n    at new Promise (<anonymous>)");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbjIwNGZlZmVmOWNmNDU1YTZkMDY5LmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7Ozs7VUNBQTtVQUNBO1VBQ0E7VUFDQTtVQUNBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vd2VicGFjay1ib2lsZXJwbGF0ZS93ZWJwYWNrL2JlZm9yZS1zdGFydHVwIiwid2VicGFjazovL3dlYnBhY2stYm9pbGVycGxhdGUvd2VicGFjay9zdGFydHVwIiwid2VicGFjazovL3dlYnBhY2stYm9pbGVycGxhdGUvd2VicGFjay9hZnRlci1zdGFydHVwIl0sInNvdXJjZXNDb250ZW50IjpbIiIsIi8vIHN0YXJ0dXBcbi8vIExvYWQgZW50cnkgbW9kdWxlIGFuZCByZXR1cm4gZXhwb3J0c1xuLy8gVGhpcyBlbnRyeSBtb2R1bGUgZG9lc24ndCB0ZWxsIGFib3V0IGl0J3MgdG9wLWxldmVsIGRlY2xhcmF0aW9ucyBzbyBpdCBjYW4ndCBiZSBpbmxpbmVkXG52YXIgX193ZWJwYWNrX2V4cG9ydHNfXyA9IHt9O1xuX193ZWJwYWNrX21vZHVsZXNfX1tcIi4vc3JjL2luZGV4LmpzXCJdKCk7XG4iLCIiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
+import 'regenerator-runtime/runtime.js';
 import './styles/main.scss';
 import logo from './assets/logo.png';
+import render from './module/render.js';
 
 const image = document.querySelector('.header__logo--img');
 
 image.src = logo;
+
+const getData = async () => {
+  const res = await fetch(
+    'https://www.themealdb.com/api/json/v1/1/filter.php?c=beef',
+  );
+  const data = await res.json();
+  console.log(data.meals);
+  await render(data.meals);
+};
+
+getData();
