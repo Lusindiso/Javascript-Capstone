e855437d0a6e667cfd20b67ceda941c18be0c172
lUCI <lusindisontanjana@gmail.com>
2022-05-19 11:39:54 +0200

    moved logo to index.js

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import logo from './assets/logo.png';
 import getLikes from './module/getLikes.js';
 import render from './module/render.js';
 
-const image = document.querySelector('.header__logo--img');
+const image = document.querySelector('.header__logo');
+image.innerHTML = `<img src="${logo}" alt="" class="header__logo--img"></img>`;
 const meals = document.querySelector('.meals');
-image.src = logo;
 const getData = async () => {
   const res = await fetch(
     'https://www.themealdb.com/api/json/v1/1/filter.php?c=beef',
