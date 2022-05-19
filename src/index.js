79ee205e58e9757d138e16340bd5a4f503982ed1
David Abol Ger <draker74@gmail.com>
2022-05-19 14:09:15 +0300

    Pop Up window with insight Linters

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ const image = document.querySelector('.header__logo--img');
 const meals = document.querySelector('.meals');
 image.src = logo;
 
-const getData = async() => {
-    const res = await fetch(
-        'https://www.themealdb.com/api/json/v1/1/filter.php?c=beef',
-    );
-    const data = await res.json();
-    render(data.meals);
-    meals.innerHTML = `Meals (${data.meals.length})`;
+const getData = async () => {
+  const res = await fetch(
+    'https://www.themealdb.com/api/json/v1/1/filter.php?c=beef',
+  );
+  const data = await res.json();
+  render(data.meals);
+  meals.innerHTML = `Meals (${data.meals.length})`;
 };
 
 getData();
@@ -21,27 +21,25 @@ getData();
 const pop = document.querySelector('.pop-up');
 
 main.addEventListener('click', (e) => {
-    e.preventDefault();
-    if (e.target.id === 'comments') {
-        pop.style.display = 'block';
-        const section = document.createElement('div');
-        const popup = document.createElement('div');
-        popup.innerHTML = `
+  e.preventDefault();
+  if (e.target.id === 'comments') {
+    pop.style.display = 'block';
+    const section = document.createElement('div');
+    const popup = document.createElement('div');
+    popup.innerHTML = `
             <h1>Comments</h1>
             <input type="text" placeholder="Your Name">
             <textarea placeholder="Your Insight"></textarea>
             <button>Submit</button>
             <button class="close">Close</button>
         `;
-        section.appendChild(popup);
-        pop.appendChild(section);
+    section.appendChild(popup);
+    pop.appendChild(section);
+  }
 
-    }
-    const close = document.querySelector('.close');
-    close.addEventListener('click', () => {
-        pop.style.display = 'none';
-        popup.remove();
-    });
-
-
+  const close = document.querySelector('.close');
+  close.addEventListener('click', (e) => {
+    e.preventDefault();
+    pop.style.display = 'none';
+  });
 });
