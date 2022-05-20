3d35f224905b57b542b4bcd1f6f13127ac81d512
lUCI <lusindisontanjana@gmail.com>
2022-05-20 10:00:57 +0200

    Add items counter

---
+++ src/test/itemCounter.test.js
@@ -0,0 +1,26 @@
+import itemsCounter from '../module/itemsCounter.js';
+
+describe('Returns number of items on the fetched from the api', () => {
+  it('returns 0 if the meals array is empty', () => {
+    const items = [];
+    expect(itemsCounter(items)).toBe(0);
+  });
+
+  it('returns the number items in the list', () => {
+    const items = [
+      {
+        strMeal: 'Beef Bourguignon',
+        idMeal: '52904',
+      },
+      {
+        strMeal: 'Beef Brisket Pot Roast',
+        idMeal: '52812',
+      },
+      {
+        strMeal: 'Beef Dumpling Stew',
+        idMeal: '52873',
+      },
+    ];
+    expect(itemsCounter(items)).toBe(3);
+  });
+});
